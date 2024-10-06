369689dfd32b93106e108fc0d68280ed6c462041
Rafli Adiputra <rafliadiputra9789@gmail.com>
2024-10-07 01:27:51 +0700

    Initial commit

---
+++ lib/testing.dart
@@ -0,0 +1,24 @@
+void main() {
+
+  print("Rafli Adiputra");
+
+  var name = "Rafli";
+
+  print(name);
+
+
+  var number1 = 5;
+  var number2 = 10;
+
+  print(number1+number2);
+
+  var namaDepan = "Rafli";
+  final namaBelakang = " Adiputra";
+
+  print(namaDepan+namaBelakang);
+
+  //ini koment
+  /*
+  ini isinya koment
+   */
+}
