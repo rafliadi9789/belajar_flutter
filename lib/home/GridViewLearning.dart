c31735716b240e6ee675aaa9249ed31834cac1ad
Rafli Adiputra <rafliadiputra9789@gmail.com>
2024-11-23 20:04:33 +0700

    Initial commit

---
+++ lib/home/GridViewLearning.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GridViewLearning extends StatelessWidget {
+  GridViewLearning({super.key});
+
+ @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.greenAccent, borderRadius: BorderRadius.circular(15)),
+      child: const Center(child: Text('Hello World')),
+      );
+  }
+}
--- lib/testing.dart
+++ lib/testing.dart
@@ -1,3 +1,4 @@
+
 void main() {
 
   print("Rafli Adiputra");
@@ -21,4 +22,82 @@ void main() {
   /*
   ini isinya koment
    */
+
+  var number_2 = 10.5;
+  double number_3 = 11.5;
+
+  print(number_3);
+  print(number_2);
+
+  num number_5 = 3;
+
+  print(number_5);
+
+  bool selesai = false;
+
+  print(selesai);
+
+  selesai = true;
+
+  print(selesai);
+
+  String namaAwal = 'Tikus';
+  String namaAkhir = 'Berdasi';
+
+  print(namaAkhir+namaAwal);
+  var namaLengkap = '$namaDepan ${namaBelakang}';
+  var namaLengkap2 = '$namaAwal ${namaAkhir}';
+
+  print(namaLengkap);
+  print(namaLengkap2);
+
+  var text = 'test 2 \$kali ';
+  print(text);
+
+  var coba_nama = namaDepan = namaBelakang;
+
+  print(coba_nama);
+
+  var longString = '''
+  ini adalah long string
+  coba berkalikali
+  biar bisa
+  aamiin
+  
+  ''';
+
+  print(longString);
+
+  ///coba dynamic
+
+  dynamic dynamic1 = 100;
+  print(dynamic1);
+
+  dynamic1 = "Dynamic";
+  print(dynamic1);
+
+  dynamic1 = true;
+  print(dynamic1);
+
+  ///coba lgi
+
+  dynamic dynamic2 = 100;
+  dynamic2 = "Dynamic";
+  dynamic2 = true;
+
+  print(dynamic2);
+  print(dynamic2);
+  print(dynamic2);
+
+  /// Konversi
+
+  var inputString = '1500';
+  var inputInt = int.parse(inputString);
+  var inputDouble = double.parse(inputString);
+
+  print(inputString);
+  print(inputInt);
+  print(inputDouble);
+
+
 }
